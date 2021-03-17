86a201c975bded349722db0d0188468288553d39
Terry Lowery <tl@lowerytech.net>
2021-03-17 01:21:08 -0500

    init repo

---
+++ src/test/java/llc/swd/microservicesdemo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package llc.swd.microservicesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
