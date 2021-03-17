902e053ea86364350d0211f97a15661c7d699031
Terry Lowery <tl@lowerytech.net>
2021-03-17 02:07:42 -0500

    Added twitter-to-kafka sub module

---
+++ twitter-to-kafka-service/src/main/java/llc/swd/microservices/twittertokafka/service/TwitterToKafakaServiceApp.java
@@ -0,0 +1,11 @@
+package llc.swd.microservices.twittertokafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafakaServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafakaServiceApp.class, args);
+    }
+}
